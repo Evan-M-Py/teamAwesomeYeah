62005dacc57a72e6eaa6450b7ba70abc96429770
Melanie Rogoff <mgrogoff@gmail.com>
2020-02-06 20:09:45 -0700

    Refactored coded for Express

--- home.js
+++ home.js
@@ -1,12 +1,16 @@
-const http = require("http");
+const express = require("express");
 
-const fs = require("fs");
+const path = require("path");
 
 //Set port to 3000
 
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(handleRequest);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Create a function for handling requests and responses coming into our server
 function handleRequest(req, res) {
@@ -14,7 +18,39 @@ function handleRequest(req, res) {
   };
 
 
-// Starts our server
-server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
-});
+
+//////////ARRAYS OF OBJECTS//////////////
+
+const reservations = [{
+    name: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  }, {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  }, {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }];
+  
+
+
+//////////////ROUTES////////
+
+
+
+
+  ///////START SERVER//////
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
